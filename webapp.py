60fcda8ea09619697507dee11be3562d673ecdc2
Sale976 <sele2203@yahoo.com>
2025-07-20 18:58:25 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import re
 
-st.set_page_config(layout="wide")
+def wide_space_default():
+    st.set_page_config(layout=“wide”)
+wide_space_default()
 
 # --- Custom Styled Title and Info Text ---
 st.markdown(
