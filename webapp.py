b6f6aeccfca4d80b493ca7b15c3a795f0d57ffbe
Sale976 <sele2203@yahoo.com>
2025-07-26 20:35:57 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -92,8 +92,8 @@ with col2:
 
                 file_link = ""
                 if acronym and page_number:
-                    timestamp = int(time.time())
-                    pdf_url = f"https://cdn.jsdelivr.net/gh/Sale976/pravilnik@main/{acronym}.pdf?v=123{timestamp}#page={page_number}"
+                    drive_file_id = "1wkVMLXFWPY8dAkam7KRbJwNkq7kBBcuy"
+                    pdf_url = f"https://drive.google.com/file/d/{drive_file_id}/preview#page={page_number}"
                     file_link = (
                         f"<a href='{pdf_url}' target='_blank' "
                         f"title='Kliknite da otvorite PDF na odgovarajućoj stranici' "
