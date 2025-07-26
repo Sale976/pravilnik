5be2d3ff863b23426e336731299b08bc952a6077
Sale976 <sele2203@yahoo.com>
2025-07-26 14:33:23 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -103,7 +103,7 @@ with col2:
                     f"""
                     <div style='display: flex; flex-direction: row; align-items: center; gap: 40px;
                                 padding: 10px; background-color: #f4f4f4;
-                                border-left: 4px solid #0077b6; margin-bottom: 15px;
+                                border-left: 4px solid #0077b6; margin-bottom: 10px;
                                 font-size:17px; flex-wrap: wrap;'>
                         <div>{match}</div>
                         <div>{file_link}</div>
