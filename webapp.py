ddef96f1c9619741d7c2ac1cd478cf1a1749a1cf
Sale976 <sele2203@yahoo.com>
2025-07-29 17:57:59 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ st.set_page_config(
 
 st.markdown("""
 <div style="display: flex; align-items: center; justify-content: center; gap: 16px; margin-bottom: 10px; margin-top: -20px;">
-    <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://pravilnikgit.streamlit.app&count_bg=%2379C83D&title_bg=%23555555&icon=streamlit.svg&icon_color=%23E7E7E7&title=posete&edge_flat=false"/>
+    <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://pravilnikgit.streamlit.app"/>
     <h2 style='margin: 0; font-size: 28px;'>
         Web aplikacija za pretragu Pravilnika o Podeli (PoPV) i Tehničkom Pregledu (PoTP)
     </h2>
