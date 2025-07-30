e6135f6ceca36aae593eb49c1bec581845dfe6bc
Sale976 <sele2203@yahoo.com>
2025-07-30 19:54:26 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -14,7 +14,7 @@ if 'show_analytics' not in st.session_state:
     st.session_state.show_analytics = False
 
 with streamlit_analytics.track():
-    st.button("Interact with me")
+    #st.button("Interact with me")
 
     # Button to toggle analytics display
     if st.button("Show Analytics"):
