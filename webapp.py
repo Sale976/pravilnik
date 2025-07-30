fc59a94d926e8839229987488de5457cca5141ff
Sale976 <sele2203@yahoo.com>
2025-07-30 14:57:48 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 import re, os
 
 
@@ -7,11 +9,25 @@ st.set_page_config(
     layout="wide"
 )
 
-ip = st.context.ip_address
-if ip is None:
-    st.write("No IP address. This is expected in local development.")
-else:
-    st.write(f"Your IP address: {ip}")
+
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
+st.title("Title")
+st.markdown(f"The remote ip is {get_remote_ip()}")
 
 # --- Title and Description ---
 st.markdown(
