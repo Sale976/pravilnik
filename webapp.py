ad8325f35922facea764058e43d012dcc3999dd4
Sale976 <sele2203@yahoo.com>
2025-07-30 17:05:33 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 with streamlit_analytics.track():
     st.write("Streamlit Analytics!")
-    st.button("Click me to see analytics!")
+    st.button("Analytics!")
 
 # --- Title and Description ---
 st.markdown(
