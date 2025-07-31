00ad031b0c7be80fa0eab61554b1f87414607fa2
Sale976 <sele2203@yahoo.com>
2025-07-31 17:48:07 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -15,7 +15,7 @@ st.markdown(
     /* Sidebar background */
     section[data-testid="stSidebar"] {
         background-color: #ADD8E6;
-        padding-top: 1rem; /* Add space at top */
+        padding-top: -5rem; /* Add space at top */
     }
     </style>
     """,
