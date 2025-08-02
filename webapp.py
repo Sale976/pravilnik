296cab76e578ae886d493cf96154b9c2ac148018
Sale976 <sele2203@yahoo.com>
2025-08-02 20:09:41 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -82,6 +82,7 @@ with bottom_placeholder.container():
     st.sidebar.write("")
     st.sidebar.write("")
     st.sidebar.write("")
+    st.sidebar.write("")
     st.sidebar.markdown("📄 **Verzija aplikacije:** 1.0.2")
     st.sidebar.markdown("🔧 *Autor: Aleksandar*")
     #st.sidebar.markdown("[💬 Prijavite grešku](mailto:aca1976@mts.rs)")
@@ -204,6 +205,7 @@ with col2:
         st.info("")
 
     st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
