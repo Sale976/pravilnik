6a4fe1b2d05475e756e5c39a290c2ac6f34725b6
Sale976 <sele2203@yahoo.com>
2025-08-02 20:03:10 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -81,6 +81,7 @@ with bottom_placeholder.container():
     st.sidebar.markdown("📄 **Verzija aplikacije:** 1.0.2")
     st.sidebar.markdown("🔧 *Autor: Aleksandar*")
     st.sidebar.markdown("[💬 Prijavite grešku](mailto:aca1976@mts.rs)")
+    st.sidebar.image("https://www.simplehitcounter.com/hit.php?uid=XXXXX&f=0&b=16777215", caption="Posete")
     
 # --- Title and Description ---
 st.markdown(
@@ -200,6 +201,7 @@ with col2:
         st.info("")
 
     st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
