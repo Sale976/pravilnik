d76ba3bc9c813316891d5d74d4879f138ab5d076
Sale976 <sele2203@yahoo.com>
2025-08-03 15:08:33 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,8 +5,8 @@ from pathlib import Path
 from datetime import datetime
 import socket
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from google.oauth2 import service_account
+#from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
 creds = st.secrets["gcp_service_account"]
 
