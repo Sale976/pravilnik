5dfe63a84673ff71a5410d81d7d945e3b3911db3
Aleksandar <sele2203@yahoo.com>
2025-09-01 19:54:04 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -68,7 +68,7 @@ st.markdown("""
 
     /* Optional: reduce padding/margin of your content */
     .sidebar-top {
-        margin-top: -9.5rem;
+        margin-top: -5.5rem;
         font-size: 16px;
     }
     </style>
