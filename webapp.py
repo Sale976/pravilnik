8d290917ce641031f35f11408e8c51604c8839d9
Aleksandar <sele2203@yahoo.com>
2025-09-01 19:49:51 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -78,7 +78,7 @@ st.markdown("""
 st.sidebar.markdown("#### 👥 Brojač Posetilaca")
 st.sidebar.markdown(f'<div style="font-size: 30px;"><strong>{count}</strong></div>', unsafe_allow_html=True)
 st.sidebar.write(f"Hvala na poseti!")
-st.sidebar.write("---")
+#st.sidebar.write("---")
 
 with st.sidebar.expander("ℹ️ Uputstvo"):
     st.markdown("""
