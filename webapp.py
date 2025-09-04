a0cba57621561dcd6f9688b117e24ac6d29dd007
Aleksandar <sele2203@yahoo.com>
2025-09-04 20:21:51 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -115,7 +115,7 @@ st.sidebar.markdown(
         list-style: none;
     }
     details div {
-        margin-top: 100px;
+        margin-top: 8px;
     }
     details[open] div {
         animation: expandUp 0.3s ease-in-out;
