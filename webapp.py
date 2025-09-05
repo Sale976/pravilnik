5565d3ef8a34078ec92e3455820dade032538c7b
Aleksandar <sele2203@yahoo.com>
2025-09-05 18:05:55 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -84,7 +84,7 @@ st.markdown("""
     
     .sidebar-bottom {
         position: absolute;
-        bottom: -250px;
+        bottom: -350px;
         left: 2px;
         right: 2px;
         background-color: #f8f9fa;
