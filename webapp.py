d5b7092f635b3594099b8f39f236294f627eedba
Aleksandar <sele2203@yahoo.com>
2025-09-05 18:03:52 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -88,11 +88,11 @@ st.markdown("""
         left: 2px;
         right: 2px;
         background-color: #f8f9fa;
-        border-top: 2px solid #0077b6;
+        border-top: 20px solid #0077b6;
         border-radius: 8px 8px 8px 8px;
         box-shadow: 0 -2px 5px rgba(0,0,0,0.5);
         font-size: 14px;
-        padding: 30px 12px;
+        padding: 5px 12px;
         margin-top: 250px;
     }
 
