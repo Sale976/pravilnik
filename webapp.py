9e845bcca26d5de26de07ffd552fed2b84f3fc6b
Aleksandar <sele2203@yahoo.com>
2025-09-05 18:13:55 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -76,7 +76,7 @@ st.markdown("""
     
     /* Container for the bottom content - FIXED POSITIONING */
     .sidebar-bottom-container {
-        margin-top: 400px;
+        margin-top: 200px;
         position: sticky;
         bottom: 0;
         width: 100%;
