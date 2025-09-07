be94517a29b73649ac344233b17882dce6489568
Aleksandar <sele2203@yahoo.com>
2025-09-07 20:31:43 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -149,7 +149,7 @@ st.markdown("""
 with st.sidebar:
     # Logo at the top
     # st.markdown('<div class="logo">📘 Pretraga Pravilnika</div>', unsafe_allow_html=True)
-    st.image("https://inspektlabs.com/blog/content/images/2020/09/Pre-Purchase_Inspection_Preview-3.jpg", width=230)
+    st.image("https://inspektlabs.com/blog/content/images/2020/09/Pre-Purchase_Inspection_Preview-3.jpg", width=240)
     
     # Container for top elements
     st.markdown('<div class="sidebar-top">', unsafe_allow_html=True)
