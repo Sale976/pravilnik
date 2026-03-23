dd7edad6fd180022642ca955d8e8010d5593d2a2
Aleksandar <sele2203@yahoo.com>
2026-03-23 17:53:03 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -233,7 +233,7 @@ st.markdown(
         Web aplikacija za pretragu Pravilnika o Podeli Motornih Vozila (PPMV) i Pravilnika o Tehničkom Pregledu Vozila (PTPV)
     </h2>
     <p style='font-size:18px; text-align: left;'>
-        🛈 Aplikacija pretražuje tekstualni fajl i na osnovu pretrage prikazuje broj člana i stranu iz PDF verzije pravilnika. Brojevi stranica važe ako su PDF fajlovi preuzeti sa zvaničnog izvora (PIS) bez izmena.
+        🛈 Aplikacija pretražuje tekstualni fajl i na osnovu pretrage prikazuje broj člana i stranu iz PDF verzije pravilnika. Brojevi stranica važe ako su PDF fajlovi preuzeti sa zvaničnog izvora (PIS) bez izmena.<br>
         🛈 Tekstualni fajl (gornje levo dugme za prikaz sadržaja) je za korisnika aplikacije isključivo informativnog karaktera!
     </p>
     </h2>
