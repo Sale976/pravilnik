2ccd9634067f21bfad431f3c7a002a43711049e2
Aleksandar <sele2203@yahoo.com>
2026-03-26 17:31:33 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -234,7 +234,7 @@ st.markdown(
     </h2>
     <p style='font-size:18px; text-align: left;'>
         🛈 Aplikacija pretražuje tekstualni fajl i na osnovu pretrage prikazuje broj člana i stranu iz PDF verzije pravilnika. Brojevi stranica važe ako su PDF fajlovi preuzeti sa zvaničnog izvora (PIS) bez izmena.<br>
-        🛈 Tekstualni fajl (gornje levo dugme za prikaz sadržaja) je za korisnika aplikacije isključivo informativnog karaktera. Preporučljivo je da korisnik letimično pregleda ovaj fajl!
+        🛈 Tekstualni fajl (gornje levo dugme za prikaz sadržaja) je za korisnika aplikacije isključivo informativnog karaktera. Preporuka je da korisnik letimično pregleda ovaj fajl!
     </p>
     </h2>
     <p style='font-size:18px; text-align: justify;'>
