81a97e187e7d40029662b8b61ca76cab83cdc2a9
Aleksandar <sele2203@yahoo.com>
2026-07-12 12:29:13 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -231,7 +231,7 @@ st.markdown(
 
 st.divider()
 
-st.write('🛈 Primer načina pretrage: Unosom reči npr. :orange["*svetlo*"] aplikacija vraća više mogućih rezultata, međutim ako se unese :orange["*kratko svetlo*"] dobija se jedan rezultat.')
+st.write('🛈 Primer načina pretrage: Unosom reči :orange["*svetlo*"] aplikacija vraća više mogućih rezultata, međutim ako se unese :orange["*kratko svetlo*"] dobija se jedan rezultat.')
 
 # --- Session State ---
 if "search_query" not in st.session_state:
